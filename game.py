df9c70de647e4c15829594e23a0702ef5889b6ee
Kornel <Kotomskikornel@gmail.com>
2026-03-15 14:46:51 +0100

    Initial commit

---
+++ game.py
@@ -0,0 +1,8 @@
+def update_game():
+    speed = 5
+    points = 0
+    food = "apple"
+    print("Start gry")
+    print("Predkosc:", speed)
+    print("Punkty:", points)
+    print("Jedzenie:", food)
